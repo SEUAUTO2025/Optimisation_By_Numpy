49cee4ba5f0cd3ebb147de58a86203cd30fff368
SEUAUTO <syxxzyyx@163.com>
2025-11-01 21:55:33 +0800

    modified:   simplex.py

---
+++ simplex.py
@@ -0,0 +1,16 @@
+import numpy as np
+class Simplex():
+    def __init__(self,n_params,C,A,b):
+        self.n_params = n_params
+        self.C = C
+        self.A = A
+        self.b = b
+    def Generate_Simplex_Table(self):
+        pass
+    def base_choose(self):
+        pass
+    def dual_simplex(self):
+        pass
+    def cal_argmin(self):
+        pass
+        
